5d027c305f9d406499cf568143af8a467ab1516f
Jonas Crevecoeur <jonas.crevecoeur@hotmail.com>
2026-07-13 18:45:18 +0200

    Add warning if Python tests are not supported

--- dbt-polars/dbt/adapters/polars/__init__.py
+++ dbt-polars/dbt/adapters/polars/__init__.py
@@ -1,12 +1,8 @@
-import dbt.parser.read_files as _read_files
 from dbt.adapters.base.plugin import AdapterPlugin
 from dbt.adapters.polars.connections import PolarsCredentials
 from dbt.adapters.polars.impl import PolarsAdapter
 from dbt.exceptions import ModelConfigError
 from dbt.include.polars import PACKAGE_PATH
-from dbt.node_types import ModelLanguage
-from dbt.parser.models import ModelParser, verify_python_model_code
-from dbt.parser.singular_test import SingularTestParser
 from dbt_common.dataclass_schema import ValidationError
 
 # dbt-core (as of 1.11.x, dbt/parser/read_files.py:get_file_types_for_project)
@@ -19,16 +15,26 @@ from dbt_common.dataclass_schema import ValidationError
 # shape: a dict of {ParseFileType: {"extensions": [...], ...}} with a mutable list.
 # If upgrading dbt-core, re-verify get_file_types_for_project still has this shape
 # before assuming this patch still applies.
-_original_get_file_types_for_project = _read_files.get_file_types_for_project
+_python_tests_patched = True
 
+try:
+    import dbt.parser.read_files as _read_files
 
-def _get_file_types_for_project_with_py_tests(project):
-    file_types = _original_get_file_types_for_project(project)
-    file_types[_read_files.ParseFileType.SingularTest]["extensions"].append(".py")
-    return file_types
+    _original_get_file_types_for_project = _read_files.get_file_types_for_project
 
+    def _get_file_types_for_project_with_py_tests(project):
+        file_types = _original_get_file_types_for_project(project)
+        try:
+            file_types[_read_files.ParseFileType.SingularTest]["extensions"].append(
+                ".py"
+            )
+        except (KeyError, AttributeError, TypeError):
+            pass
+        return file_types
 
-_read_files.get_file_types_for_project = _get_file_types_for_project_with_py_tests
+    _read_files.get_file_types_for_project = _get_file_types_for_project_with_py_tests
+except (ImportError, AttributeError):
+    _python_tests_patched = False
 
 # dbt-core populates `node.refs`/`node.sources` (and, downstream, `depends_on.nodes`)
 # for Python *models* by statically AST-scanning their source for ref()/source()/
@@ -58,29 +64,47 @@ _read_files.get_file_types_for_project = _get_file_types_for_project_with_py_tes
 # which forwards to the user's `test`.
 _TEST_ENTRYPOINT_WRAPPER = "\n\n\ndef model(dbt, pl):\n    return test(dbt, pl)\n"
 
-_original_singular_test_render_update = SingularTestParser.render_update
+try:
+    from dbt.node_types import ModelLanguage
+    from dbt.parser.models import ModelParser, verify_python_model_code
+    from dbt.parser.singular_test import SingularTestParser
 
+    _original_singular_test_render_update = SingularTestParser.render_update
 
-def _singular_test_render_update(self, node, config, validate_config_call_dict=False):
-    if node.language == ModelLanguage.python:
-        if not node.raw_code.endswith(_TEST_ENTRYPOINT_WRAPPER):
-            node.raw_code += _TEST_ENTRYPOINT_WRAPPER
-        try:
-            verify_python_model_code(node)
-            context = self._context_for(node, config)
-            ModelParser.parse_python_model(self, node, config, context)
-            self.update_parsed_node_config(
-                node, config, context=context, validate_config_call_dict=True
-            )
-        except ValidationError as exc:
-            raise ModelConfigError(exc, node=node) from exc
-        return
-    return _original_singular_test_render_update(
-        self, node, config, validate_config_call_dict
+    def _singular_test_render_update(
+        self, node, config, validate_config_call_dict=False
+    ):
+        if node.language == ModelLanguage.python:
+            if not node.raw_code.endswith(_TEST_ENTRYPOINT_WRAPPER):
+                node.raw_code += _TEST_ENTRYPOINT_WRAPPER
+            try:
+                verify_python_model_code(node)
+                context = self._context_for(node, config)
+                ModelParser.parse_python_model(self, node, config, context)
+                self.update_parsed_node_config(
+                    node, config, context=context, validate_config_call_dict=True
+                )
+            except ValidationError as exc:
+                raise ModelConfigError(exc, node=node) from exc
+            except Exception:
+                return _original_singular_test_render_update(
+                    self, node, config, validate_config_call_dict
+                )
+            return
+        return _original_singular_test_render_update(
+            self, node, config, validate_config_call_dict
+        )
+
+    SingularTestParser.render_update = _singular_test_render_update  # type: ignore[method-assign]
+except (ImportError, AttributeError):
+    _python_tests_patched = False
+
+if not _python_tests_patched:
+    from dbt.adapters.events.logging import AdapterLogger
+
+    AdapterLogger("polars").warning(
+        "Python singular tests are not supported with this version of dbt-core."
     )
-
-
-SingularTestParser.render_update = _singular_test_render_update  # type: ignore[method-assign]
 
 Plugin = AdapterPlugin(
     adapter=PolarsAdapter,  # type: ignore[arg-type]
